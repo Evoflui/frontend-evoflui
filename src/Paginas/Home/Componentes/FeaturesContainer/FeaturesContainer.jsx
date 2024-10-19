11852ec5f9bdad964230a72a6fdbfc8a2596e1ce
Gustavo Portela <126726122+portelagu@users.noreply.github.com>
2024-10-19 12:38:18 +0000

    start features-container component

---
+++ src/Paginas/Home/Componentes/FeaturesContainer/FeaturesContainer.jsx
@@ -0,0 +1,19 @@
+import './FeaturesContainer.css'
+
+function FeaturesContainer() {
+    return (
+        <div>
+            <main className="features-container">
+                <section className="right-content">
+                    <div className="top-info">
+                        <img src="" alt="ícone de um" />
+                        <h1 className="title-info">Trilha de Desenvolvimento</h1>
+                    </div>
+                    <p className="text-info">Oferecemos uma trilha de desenvolvimento social para pessoas autistas e, para os tutores, uma trilha focada na conscientização sobre a neurodivergência, com recursos práticos de apoio.</p>   
+                </section>
+            </main>
+        </div>
+    )
+}
+
+export default FeaturesContainer;
