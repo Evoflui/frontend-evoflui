b1c434596f5f866f7e908c37383bc2f6c50a575f
Gustavo Portela <126726122+portelagu@users.noreply.github.com>
2024-10-19 21:28:43 +0000

    finish FeaturesContainer

--- src/Paginas/Home/Componentes/FeaturesContainer/FeaturesContainer.jsx
+++ src/Paginas/Home/Componentes/FeaturesContainer/FeaturesContainer.jsx
@@ -1,4 +1,7 @@
 import './FeaturesContainer.css'
+import LampIcon from '../../../../assets/icones/lampicon.svg'
+import PeopleIcon from '../../../../assets/icones/peopleIcon.svg'
+import ConfettiIcon from '../../../../assets/icones/confettiIcon.svg'
 
 function FeaturesContainer() {
     return (
@@ -6,11 +9,30 @@ function FeaturesContainer() {
             <main className="features-container">
                 <section className="right-content">
                     <div className="top-info">
-                        <img src="" alt="ícone de um" />
+                        <img src={LampIcon} alt="ícone de uma lâmpada" />
                         <h1 className="title-info">Trilha de Desenvolvimento</h1>
                     </div>
-                    <p className="text-info">Oferecemos uma trilha de desenvolvimento social para pessoas autistas e, para os tutores, uma trilha focada na conscientização sobre a neurodivergência, com recursos práticos de apoio.</p>   
+                    <p className="text-info">Oferecemos uma trilha de desenvolvimento social para pessoas autistas e, para os tutores, uma trilha focada na conscientização sobre a neurodivergência, com recursos práticos de apoio.</p>
                 </section>
+
+                <section className="left-content">
+                    <div className="top-info">
+                        <img src={ConfettiIcon} alt="ícone de várias pessoas em conjuntos" />
+                        <h1 className="title-info">Fórum de Tutores</h1>
+                    </div>
+                    <p className="text-info">Um fórum exclusivo para tutores, onde é possível trocar
+                        experiências e dicas sobre o acompanhamento de
+                        crianças neurodivergentes.</p>
+                </section>
+
+                <section className="second-right-content">
+                    <div className="top-info">
+                        <img src={PeopleIcon} alt="ícone de confetti" />
+                        <h1 className="title-info">Eventos Especiais</h1>
+                    </div>
+                    <p className="text-info">Promovemos e divulgamos encontros especializados voltados para o autismo, visando o engajamento e a troca de experiências.</p>
+                </section>
+
             </main>
         </div>
     )
