12fe9115a6eb12ecc0f0ff691ea84985b21640a2
Gustavo Portela <126726122+portelagu@users.noreply.github.com>
2024-10-21 12:48:58 +0000

    add routes to pages

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import Home from "./Paginas/Home/Home";
+import SobreNos from './Paginas/SobreNos/SobreNos'; 
+import { Routes, Route } from 'react-router-dom'; 
 
 function App() {
   return (
     <div className="App">
-      <Home/>
+      <Routes>
+        <Route path="/" element={<Home />} /> 
+        <Route path="/sobre-nos" element={<SobreNos />} />
+      </Routes>
     </div>
   );
 }
---
+++ src/Paginas/SobreNos/SobreNos.jsx
@@ -0,0 +1,12 @@
+import Header from '../../ComponentesGerais/Header/Header';
+import './SobreNos.css';
+
+function SobreNos(){
+    return (
+        <div>
+            <Header/>
+        </div>
+    )
+}
+
+export default SobreNos;
