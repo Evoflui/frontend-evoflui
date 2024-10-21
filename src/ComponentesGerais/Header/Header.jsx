3a085335261f4d485e8afefa8b5fb5095e15a6cd
Gustavo Portela <126726122+portelagu@users.noreply.github.com>
2024-10-21 12:48:58 +0000

    add routes to pages

--- src/ComponentesGerais/Header/Header.jsx
+++ src/ComponentesGerais/Header/Header.jsx
@@ -1,21 +1,22 @@
 import React from 'react';
 import './Header.css';
-import Logo from '../../assets/icones/logo.svg'
+import Logo from '../../assets/icones/logo.svg';
+import { Link } from 'react-router-dom'; 
 
-function Header(){
-    return(
+function Header() {
+    return (
         <div>
             <section id="header-section">
                 <img src={Logo} alt="Logomarca do evoflui" />
                 <div id="lista">
-                    <li id="lista-item"><a href="#" id="lista-item-link">Início</a></li>
-                    <li id="lista-item"><a href="#" id="lista-item-link">Sobre Nós</a></li>
-                    <li id="lista-item"><a href="#" id="lista-item-link">Fale Conosco</a></li>
-                    <li id="lista-item"><a href="#" id="lista-item-botao">Comece agora</a></li>
+                    <li id="lista-item"><Link to="/" id="lista-item-link">Início</Link></li>
+                    <li id="lista-item"><Link to="/sobre-nos" id="lista-item-link">Sobre Nós</Link></li>
+                    <li id="lista-item"><Link to="/fale-conosco" id="lista-item-link">Fale Conosco</Link></li>
+                    <li id="lista-item"><Link to="/comece-agora" id="lista-item-botao">Comece agora</Link></li>
                 </div>
             </section>
         </div>
-    )
+    );
 }
 
 export default Header;
