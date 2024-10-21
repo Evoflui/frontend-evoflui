211aab0feac37471c8ecca9077a93560c0d267f4
Gustavo Portela <126726122+portelagu@users.noreply.github.com>
2024-10-21 21:45:22 +0000

    add routes to footer | create new pages

---
+++ src/Paginas/FaleConosco/FaleConosco.jsx
@@ -0,0 +1,11 @@
+import Header from "../../ComponentesGerais/Header/Header";
+
+function FaleConosco(){
+    return(
+        <div>
+            <Header/>
+        </div>
+    )
+}
+
+export default FaleConosco;
---
+++ src/Paginas/TermosDeUso/TermosDeUso.jsx
@@ -0,0 +1,11 @@
+import Header from "../../ComponentesGerais/Header/Header";
+
+function TermosDeUso(){
+    return(
+        <div>
+            <Header/>
+        </div>
+    )
+}
+
+export default TermosDeUso;
