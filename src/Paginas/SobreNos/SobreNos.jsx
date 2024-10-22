3810c453737f47f579d28efdeaebf060d3873e1a
Caroline Fernandes <78885182+Carolzin@users.noreply.github.com>
2024-10-22 11:56:06 +0000

    add sobre nos

--- src/Paginas/SobreNos/SobreNos.jsx
+++ src/Paginas/SobreNos/SobreNos.jsx
@@ -1,10 +1,14 @@
 import Header from '../../ComponentesGerais/Header/Header';
+import QuemSomos from './Componentes/QuemSomos/QuemSomos'
+import Valores from './Componentes/Valores/Valores';
 import './SobreNos.css';
 
 function SobreNos(){
     return (
         <div>
             <Header/>
+            <QuemSomos/>
+            <Valores/>
         </div>
     )
 }
