add2d3e2b5fe93d94cab828cc289c041f04071d5
Gustavo Portela <126726122+portelagu@users.noreply.github.com>
2024-10-22 15:14:16 +0000

    add LeftPadding to InformationContainer

--- src/Paginas/Pagamento/Pagamento.jsx
+++ src/Paginas/Pagamento/Pagamento.jsx
@@ -1,9 +1,11 @@
+import Footer from "../../ComponentesGerais/Footer/Footer";
 import Header from "../../ComponentesGerais/Header/Header";
 
 function Pagamento(){
     return(
         <div>
             <Header/>
+            <Footer/>
         </div>
     )
 }
