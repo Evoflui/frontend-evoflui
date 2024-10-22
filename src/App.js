d3c21c952fce93a406823ccb10555721e90c85b2
Gustavo Portela <126726122+portelagu@users.noreply.github.com>
2024-10-22 14:46:03 +0000

    add routes to planContainer elements / create Pagamento container

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import SobreNos from './Paginas/SobreNos/SobreNos';
 import FaleConosco from './Paginas/FaleConosco/FaleConosco'
 import TermosDeUso from './Paginas/TermosDeUso/TermosDeUso';
 import PoliticaDePrivacidade from './Paginas/PoliticaDePrivacidade/PoliticaDePrivacidade'
+import Pagamento from './Paginas/Pagamento/Pagamento'
 import { Routes, Route } from 'react-router-dom'; 
 
 function App() {
@@ -15,6 +16,7 @@ function App() {
         <Route path="/fale-conosco" element={<FaleConosco />} />
         <Route path="/termos-de-uso" element={<TermosDeUso />} />
         <Route path="/politica-de-privacidade" element={<PoliticaDePrivacidade />} />
+        <Route path="/pagamento" element={<Pagamento/>}/>
       </Routes>
     </div>
   );
