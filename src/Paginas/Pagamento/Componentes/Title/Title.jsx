97ffae14b140513ca15817cd37639fbca3394b1a
Gustavo Portela <126726122+portelagu@users.noreply.github.com>
2024-10-23 17:30:36 +0000

    start Payment page components

---
+++ src/Paginas/Pagamento/Componentes/Title/Title.jsx
@@ -0,0 +1,14 @@
+import './Title.css'
+
+function Title(){
+    return(
+        <div>
+            <section className='payment-section-top'>
+                <h1 className="payment-section-title">Pagamento</h1>
+                <p className='payment-section-subtitle'>Ficamos felizes por você considerar ajudar a nossa causa!</p>
+            </section>
+        </div>
+    )
+}
+
+export default Title;
--- src/Paginas/Pagamento/Pagamento.jsx
+++ src/Paginas/Pagamento/Pagamento.jsx
@@ -1,10 +1,12 @@
 import Footer from "../../ComponentesGerais/Footer/Footer";
 import Header from "../../ComponentesGerais/Header/Header";
+import Title from "./Componentes/Title/Title";
 
 function Pagamento(){
     return(
         <div>
             <Header/>
+            <Title/>
             <Footer/>
         </div>
     )
