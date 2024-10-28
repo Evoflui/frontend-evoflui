9f282a485045a6cb8078886ba995be05728c8001
Gustavo Portela <126726122+portelagu@users.noreply.github.com>
2024-10-28 17:11:38 +0000

    fix home page

--- src/Paginas/Home/Componentes/InformationContainer/InformationContainer.jsx
+++ src/Paginas/Home/Componentes/InformationContainer/InformationContainer.jsx
@@ -8,7 +8,7 @@ function InformationContainer() {
                 <img src={AutisticFriends} alt="Dois jovens autistas interagindo e sorrindo entre si"/>
                 <div className="info-container">
                     <h1 className='info-title'>Transtorno do Espectro Autista</h1>
-                    <p className="info-text">É uma condição neurológica que afeta o desenvolvimento social, a comunicação e o comportamento. Pessoas com TEA podem ter diferentes formas de pensar e interagir com o mundo, e suas características variam amplamente de uma pessoa para outra. O suporte adequado e a inclusão são essenciais para garantir seu bem-estar.</p>
+                    <p className="info-text">Transtorno do Espectro Autista, ou TEA, é uma condição neurológica que afeta o desenvolvimento social, a comunicação e o comportamento. Pessoas com TEA podem ter diferentes formas de pensar e interagir com o mundo, e suas características variam amplamente de uma pessoa para outra. O suporte adequado e a inclusão são essenciais para garantir seu bem-estar.</p>
                     <a href="https://www.gov.br/saude/pt-br/assuntos/noticias/2022/abril/tea-saiba-o-que-e-o-transtorno-do-espectro-autista-e-como-o-sus-tem-dado-assistencia-a-pacientes-e-familiares" className="info-button" target="blank">Saber mais</a>
                 </div>
             </section>
--- src/Paginas/Home/Componentes/UserFeedbacks/UserFeedbacks.jsx
+++ src/Paginas/Home/Componentes/UserFeedbacks/UserFeedbacks.jsx
@@ -22,37 +22,55 @@ function UserFeedbacks() {
             imagem: Person1,
             descricao: 'Imagem de uma mulher negra com coroa de flores',
             nome: 'Alissa Varela',
-            depoimento: 'A trilha de conscientização para tutores é excelente. Os recursos fornecidos ajudam a entender as necessidades dessas pessoas. A abordagem da plataforma é prática e acessível, e os eventos sociais são um bônus valioso para todos.'
+            depoimento: 'A trilha de conscientização para tutores é realmente excelente. Os recursos fornecidos ajudam não só a entender as necessidades dessas pessoas, mas também a se conectar com suas experiências. A abordagem da plataforma é prática e acessível, tornando o aprendizado mais fluido. E os eventos sociais? Eles são um bônus valioso para todos! Proporcionam momentos de troca e apoio que enriquecem a jornada de cada um.'
         },
         {
             id: '2',
             imagem: Person2,
             descricao: 'Imagem de uma mulher negra com um chapéu de praia branco',
             nome: 'Marina Fiori',
-            depoimento: 'O evento sobre TEA que participei foi muito informativo e organizado. A possibilidade de interagir com outros pais e especialistas foi enriquecedora. A plataforma é uma excelente ferramenta para quem busca apoio e recursos.'
+            depoimento: 'O evento sobre o autismo que eu tive a oportunidade de participar foi extremamente informativo e muito bem organizado pelas pessoas. A chance de interagir com outros pais e especialistas foi muito enriquecedora para mim e me fez sentir mais conectada. A plataforma se destaca como uma excelente ferramenta para quem busca apoio e recursos, oferecendo tudo que precisamos para entender um pouco melhor.'
         },
         {
             id: '3',
             imagem: Person3,
             descricao: 'Imagem de uma homem branco usando óculos',
             nome: 'Dante Ferraz',
-            depoimento: 'A plataforma foi essencial para o desenvolvimento social do meu filho. As trilhas de aprendizado são incríveis e realmente fazem a diferença no dia a dia. O fórum é uma ótima opção para aqueles que querem trocar experiências e conhecimento.'
+            depoimento: 'A plataforma foi fundamental para o desenvolvimento social e cognitivo do João, meu filho que possui TEA. As trilhas de aprendizado são incríveis e realmente fazem a diferença no nosso dia a dia, proporcionando um aprendizado contínuo e significativo. O fórum também se mostroy uma ótima opção para aqueles que buscam trocar experiências e conhecimento, como eu, criando um espaço de apoio mútuo.'
         },
         {
             id: '4',
             imagem: Person4,
             descricao: 'Imagem de uma homem branco usando óculos',
             nome: 'Lucas Andrade',
-            depoimento: 'Os conteúdos oferecidos pela plataforma são enriquecedores. As trilhas são bem planejadas e os eventos proporcionam uma ótima chance de conexão com outros pais e especialistas. É um espaço seguro e acolhedor para aprender.'
+            depoimento: 'Os conteúdos oferecidos pela plataforma são enriquecedores e contribuem muito para nosso aprendizado. As trilhas são bem planejadas, permitindo notar a eficácia do desenvolvimento. Além disso, os eventos criam uma ótima oportunidade de conexão com especialistas e pessoas iguais a mim, formando uma rede de apoio. É um espaço seguro e acolhedor para todos que desejam aprender e crescer juntos.'
         },
         {
             id: '5',
             imagem: Person5,
             descricao: 'Imagem de uma mulher negra utilizando uma regata branca',
             nome: 'Isabela Duarte',
-            depoimento: 'A jornada de aprendizado da plataforma superou minhas expectativas. As ferramentas são fáceis de usar e ajudaram minha filha a se desenvolver. A comunidade de tutores é acolhedora, sempre compartilhando experiências.'
+            depoimento: 'A jornada de aprendizado na plataforma realmente superou minhas expectativas de várias maneiras. As ferramentas disponíveis são muito fáceis de usar e têm contribuído significativamente para o desenvolvimento da minha filha adolescente. Além disso, a comunidade de tutores é incrivelmente acolhedora, sempre disposta a compartilhar experiências e apoiar uns aos outros. Essa troca faz toda a diferença.'
         }
     ]
+
+    var userContainers = document.querySelectorAll('.user-container');
+    function setEqualHeight() {
+        let maxHeight = 0;
+    
+        userContainers.forEach(container => {
+            container.style.height = 'auto'; 
+            maxHeight = Math.max(maxHeight, container.offsetHeight);
+        });
+    
+        userContainers.forEach(container => {
+            container.style.height = `${maxHeight}px`; 
+        });
+    }
+    
+    window.onload = setEqualHeight;
+    window.onresize = setEqualHeight; 
+    
     return (
         <div>
             <h1 className='section-title'>Nossas últimas avaliações</h1>
