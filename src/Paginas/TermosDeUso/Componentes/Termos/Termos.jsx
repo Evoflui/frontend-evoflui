a2dd00f0c0280ee048cb847b23cb1ad3e0f8048d
Carolzin <carol-28.01@hotmail.com>
2024-11-01 16:10:22 -0300

    add comp termos

---
+++ src/Paginas/TermosDeUso/Componentes/Termos/Termos.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import './Termos.css';
+
+export function Termos() {
+    return (
+        <>
+
+            <div className='container'>
+                <p className='texto-termos'> Termos de Uso </p>
+            </div>
+
+            <div className='container-atualizacao'>
+                <p className='texto-atualizacao'> Última Atualização em 12/2024</p>
+            </div>
+
+        </>
+    )
+}
+
+export default Termos;
--- src/Paginas/TermosDeUso/TermosDeUso.jsx
+++ src/Paginas/TermosDeUso/TermosDeUso.jsx
@@ -1,9 +1,12 @@
 import Header from "../../ComponentesGerais/Header/Header";
+import Termos from './Componentes/Termos/Termos';
+
 
 function TermosDeUso(){
     return(
         <div>
             <Header/>
+            <Termos/>
         </div>
     )
 }
