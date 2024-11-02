836e0a3acc77fe7f3213e52945398dfafbb39af3
Nicole M <marcondesnicole4@gmail.com>
2024-11-02 20:13:17 -0300

    Política de Privacidade adicionado

---
+++ src/Paginas/PoliticaDePrivacidade/Componentes/Politica/Politica.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import './Politica.css';
+
+export function PoliticaDePrivacidade() {
+    return (
+        <>
+
+            <div className='caixa'>
+                <p className='texto-politica'> Política de Privacidade </p>
+            </div>
+
+            <div className='caixa-atualizacao'>
+                <p className='atualizacao'> Última Atualização em 12/2024</p>
+            </div>
+
+        </>
+    )
+}
+
+export default PoliticaDePrivacidade;
