8502197566af6e42a325d6f38f9292c62f6baa4d
Nicole M <marcondesnicole4@gmail.com>
2024-11-02 20:13:17 -0300

    Política de Privacidade adicionado

--- src/Paginas/PoliticaDePrivacidade/PoliticaDePrivacidade.jsx
+++ src/Paginas/PoliticaDePrivacidade/PoliticaDePrivacidade.jsx
@@ -1,11 +1,18 @@
 import Header from "../../ComponentesGerais/Header/Header";
+import Politica from "./Componentes/Politica/Politica";
+import Privacidade from "./Componentes/Privacidade/Privacidade";
+import Footer from '../../ComponentesGerais/Footer/Footer';
 
 function PoliticaDePrivacidade(){
     return(
         <div>
             <Header/>
+            <Politica/>
+            <Privacidade/>
+            <Footer/>
+            
         </div>
-    )
-}
+    );
+};
 
 export default PoliticaDePrivacidade;
