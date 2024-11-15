fb5e2b6a608be7be35b315a8f04d34850b9f4172
Gustavo Portela <ggomesportela26@gmail.com>
2024-11-15 01:00:44 -0300

    update login page

--- src/Paginas/Cadastro/Cadastro.jsx
+++ src/Paginas/Cadastro/Cadastro.jsx
@@ -1,5 +1,6 @@
 import ImageLogin from '../../assets/imagens/imagemdelogin.svg'
 import ImageSignUp from '../../assets/imagens/imagemderegistro.svg'
+import './Cadastro.css'
 
 function Cadastro(){
 return(
@@ -7,12 +8,20 @@ return(
         <div className="login-signup-container">
             <div className="forms-login-signup-container">
                 <div className="login-signup-div">
+
                     <form action="#" className="login-form">
                         <h2 className="login-title">Acesso</h2>
                         <div className="login-input-field">
-                            <i className='fas fa-user'></i>
+                            <input type='email' placeholder='E-mail'/>
                         </div>
+
+                        <div className="login-input-field">
+                            <input type='password' placeholder='Senha'/>
+                        </div>
+
+                        <input type="submit" value="Entrar" className='btn solid'/>
                     </form>
+
                 </div>
                 <div className="panels-login-signup-container">
 
