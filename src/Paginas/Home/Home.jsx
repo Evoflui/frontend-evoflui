9d652a67aeae7e6be009f7e5a2547130943264cb
Carolzin <carol-28.01@hotmail.com>
2024-11-15 22:31:50 -0300

    add dashboard e rodape

--- src/Paginas/Home/Home.jsx
+++ src/Paginas/Home/Home.jsx
@@ -1,6 +1,7 @@
 import Header from '../../ComponentesGerais/Header/Header';
 import Footer from '../../ComponentesGerais/Footer/Footer';
 import BoasVindas from './Componentes/BoasVindas/BoasVindas';
+import DashBoard from './Componentes/DashBoard/DashBoard';
 
 
 function Home(){
@@ -8,6 +9,8 @@ function Home(){
         <div>
             <Header/>
             <BoasVindas/>
+            <DashBoard/>
+            <Footer/>
            
            
         </div>
