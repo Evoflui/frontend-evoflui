c567a5695a1714e7478835c7bf1ab8be8aecef18
Josineudo Arruda <96135176+josineudo-arruda@users.noreply.github.com>
2024-11-16 18:02:54 -0300

    merge no header, em li

--- src/ComponentesGerais/Header/Header.jsx
+++ src/ComponentesGerais/Header/Header.jsx
@@ -9,11 +9,7 @@ function Header() {
             <section id="header-section">
                 <img src={Logo} alt="Logomarca do evoflui" />
                 <div id="lista">
-<<<<<<< HEAD
-                    <li id="lista-item"><Link to="/" id="lista-item-link">Início</Link></li>
-=======
                     <li id="lista-item"><Link to="/home" id="lista-item-link">Início</Link></li>
->>>>>>> 02112b447b201d3efcbc7e5c52d063049b8e2a31
                     <li id="lista-item"><Link to="/sobre-nos" id="lista-item-link">Sobre Nós</Link></li>
                     <li id="lista-item"><Link to="/fale-conosco" id="lista-item-link">Fale Conosco</Link></li>
                     <li id="lista-item"><Link to="/comece-agora" id="lista-item-botao">Comece agora</Link></li>
