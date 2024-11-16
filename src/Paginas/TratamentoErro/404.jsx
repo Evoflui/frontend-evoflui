679859cb9145b2e8a5bf607c45acf21616203abc
Josineudo Arruda <96135176+josineudo-arruda@users.noreply.github.com>
2024-11-16 17:53:45 -0300

    add: login e cadastro com bdd

---
+++ src/Paginas/TratamentoErro/404.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import './Erro.css'; // Arquivo de estilo
+
+const ErrorPage = () => {
+    return (
+        <div className="error-container">
+            <div className="error-content">
+                <h1 className="error-code">404</h1>
+                <p className="error-message">Oops! A página que você está procurando não foi encontrada.</p>
+                <button className="error-button" onClick={() => window.location.href = '/'}>Voltar para a Home</button>
+            </div>
+        </div>
+    );
+}
+
+export default ErrorPage;
